74d261221a676cd96e6f50b5ce027a626c3d2c8b
Shimary_Poojani <shimaryfernando@gmail.com>
2021-09-02 15:35:05 +0530

    added new variable called address

--- MCAT/Entities/Customer.cs
+++ MCAT/Entities/Customer.cs
@@ -4,7 +4,9 @@ namespace MCAT.Entities
     class Customer
     {
         private int cid;
+        private string address;
 
         public int Cid { get => cid; set => cid = value; }
+        public string Address { get => address; set => address = value; }
     }
 }
