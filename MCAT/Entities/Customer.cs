0874651b690512c844b9f65b7a3504eac91d6e24
Oshani Kaushalya <oshani110@gmail.com>
2021-09-02 15:54:34 +0530

    Add customer last name to the customer

--- MCAT/Entities/Customer.cs
+++ MCAT/Entities/Customer.cs
@@ -5,8 +5,13 @@ namespace MCAT.Entities
     {
         private int cid;
         private string address;
+        private string lname;
+        
+        
 
         public int Cid { get => cid; set => cid = value; }
         public string Address { get => address; set => address = value; }
+        public string Lname { get => lname; set => lname = value;}
+
     }
 }
