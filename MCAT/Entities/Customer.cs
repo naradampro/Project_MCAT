ca96de562d353362bb7d343bb18265849be9f238
Narada Abeykoon <naradampro@gmail.com>
2021-09-02 15:25:26 +0530

    Customer entity cleard.

--- MCAT/Entities/Customer.cs
+++ MCAT/Entities/Customer.cs
@@ -3,10 +3,6 @@ namespace MCAT.Entities
 {
     class Customer
     {
-        private int id;
-        private string fname;
-
-        public int Id { get => id; set => id = value; }
-        public string Fname { get => fname; set => fname = value; }
+        
     }
 }
