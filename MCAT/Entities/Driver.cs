7eb257d3d443927c36604064d347ba5d6bd51694
Oshani Kaushalya <oshani110@gmail.com>
2021-09-17 11:00:00 +0530

    Driver

--- MCAT/Entities/Driver.cs
+++ MCAT/Entities/Driver.cs
@@ -36,17 +36,7 @@ namespace MCAT.Entities
         //Driver Status
         private int dstatus;
 
-        public int Id { get => id; set => id = value; }
-        public string Address { get => address; set => address = value; }
-        public int Mobileno { get => mobileno; set => mobileno = value; }
-        public string Nic { get => nic; set => nic = value; }
-        public string Fname { get => fname; set => fname = value; }
-        public string Lname { get => lname; set => lname = value; }
-        public string Bdate { get => bdate; set => bdate = value; }
-        public int Lid { get => lid; set => lid = value; }
-        public string Lictype { get => lictype; set => lictype = value; }
-        public string Licexdate { get => licexdate; set => licexdate = value; }
-        public int Dstatus { get => dstatus; set => dstatus = value; }
+        
 
 
         //default constructors
@@ -87,5 +77,18 @@ namespace MCAT.Entities
             this.licexdate=licexdate;
             this.dstatus=dstatus;
         }
+
+
+        public int Id { get => id; set => id = value; }
+        public string Address { get => address; set => address = value; }
+        public int Mobileno { get => mobileno; set => mobileno = value; }
+        public string Nic { get => nic; set => nic = value; }
+        public string Fname { get => fname; set => fname = value; }
+        public string Lname { get => lname; set => lname = value; }
+        public string Bdate { get => bdate; set => bdate = value; }
+        public int Lid { get => lid; set => lid = value; }
+        public string Lictype { get => lictype; set => lictype = value; }
+        public string Licexdate { get => licexdate; set => licexdate = value; }
+        public int Dstatus { get => dstatus; set => dstatus = value; }
     }
 }
