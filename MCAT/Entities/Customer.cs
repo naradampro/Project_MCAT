71f29f9a74aa79f74a9e233894d619a01d102be4
Oshani Kaushalya <oshani110@gmail.com>
2021-09-17 11:00:48 +0530

    Customer

--- MCAT/Entities/Customer.cs
+++ MCAT/Entities/Customer.cs
@@ -26,13 +26,6 @@ namespace MCAT.Entities
         
         
 
-        public int Id { get => id; set => id = value; }
-        public string Address { get => address; set => address = value; }
-        public string Fname { get => fname; set => fname = value;}
-        public string Lname { get => lname; set => lname = value;}
-        public int Mobileno { get => mobileno; set => mobileno = value; }
-        public int Cstatus { get => cstatus; set => cstatus = value; }
-        public string Nic { get => nic; set => nic = value; }
 
         //default constructor
         public Customer()
@@ -63,6 +56,15 @@ namespace MCAT.Entities
             this.cstatus=cstatus;
             this.nic=nic;
         }
+
+        
+        public int Id { get => id; set => id = value; }
+        public string Address { get => address; set => address = value; }
+        public string Fname { get => fname; set => fname = value;}
+        public string Lname { get => lname; set => lname = value;}
+        public int Mobileno { get => mobileno; set => mobileno = value; }
+        public int Cstatus { get => cstatus; set => cstatus = value; }
+        public string Nic { get => nic; set => nic = value; }
     }
 
     
