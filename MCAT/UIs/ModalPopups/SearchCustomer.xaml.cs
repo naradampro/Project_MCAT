d2d27da3701a42e6de844469f528e1bcc1c71fe2
jshalloluwa <jayoda.shalloluwa@gmail.com>
2021-09-29 10:53:38 +0530

    Added Three Pages.

---
+++ MCAT/UIs/ModalPopups/SearchCustomer.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MCAT.UIs.ModalPopups
+{
+    /// <summary>
+    /// Interaction logic for SearchCustomer.xaml
+    /// </summary>
+    public partial class SearchCustomer : Page
+    {
+        public SearchCustomer()
+        {
+            InitializeComponent();
+        }
+    }
+}
